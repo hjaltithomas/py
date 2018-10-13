885f71a04142977d107b8433bde55c103e1e160b
antoniedoru <doru0038@stud.kea.dk>
2018-10-13 19:26:19 +0200

    added new class

--- pypypy.py
+++ pypypy.py
@@ -3,4 +3,9 @@ print("print")
 #class
 
 def __init__
-    
+
+class ClassName(object):
+    """docstring for ."""
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
